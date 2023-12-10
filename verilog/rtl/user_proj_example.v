ea20b9f0aba874dabbe80114b785882099a02ea6
Steven Conaway <steven.conaway@icloud.com>
2023-12-10 22:46:27 +0000

    get synthesis passing

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -61,7 +61,8 @@ module hangy(input  logic             clk, reset,
   logic guessed_letters_is_done;
   logic tries_eq_7;
   logic s_tries, en_tries;
-  logic [2:0] s_guessed_letters, en_guessed_letters;
+  logic [2:0] s_guessed_letters;
+  logic en_guessed_letters;
   logic en_word_index;
   logic en_input_char;
   logic s_win, en_win;
@@ -130,13 +131,14 @@ module hangy(input  logic             clk, reset,
 
 endmodule
 
-module controller(input logic clk, reset, 
+module controller(input  logic clk, reset, 
                   input  logic next, 
                   input  logic [4:0] input_char_eq_word,
                   input  logic guessed_letters_is_done,
                   input  logic tries_eq_7, 
                   output logic s_tries, en_tries,
-                  output logic [2:0] s_guessed_letters, en_guessed_letters,
+                  output logic [2:0] s_guessed_letters, 
+                  output logic en_guessed_letters,
                   output logic en_word_index,
                   output logic en_input_char,
                   output logic s_win, en_win,
@@ -274,7 +276,8 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
                  input   logic [5:0] chip_input,
 
                   input logic s_tries, en_tries,
-                  input logic [2:0] s_guessed_letters, en_guessed_letters,
+                  input logic [2:0] s_guessed_letters,
+                  input logic en_guessed_letters,
                   input logic en_word_index,
                   input logic en_input_char,
                   input logic s_win, en_win,
@@ -292,7 +295,7 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
 
   reg [2:0] tries;
 
-  always_comb 
+  always_ff @(posedge clk) 
     begin
       if (en_tries) begin
         if (s_tries) tries = tries + 1'b1;
@@ -300,7 +303,7 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
       end
     end
   
-  always_comb
+  always_ff @(posedge clk)
     begin
       if (en_guessed_letters) begin
         case (s_guessed_letters)
@@ -317,7 +320,7 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
 
   wire [5:0] random6;
   reg [5:0] word_index;
-  always_comb
+  always_ff @(posedge clk)
     if (en_word_index) word_index = random6;
   
 
@@ -336,7 +339,7 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
   );
 
   reg [4:0] input_char;
-  always_comb
+  always_ff @(posedge clk)
     if (en_input_char) begin 
       input_char = chip_input[4:0];
       // $display("input_char: %b", input_char);
@@ -358,10 +361,10 @@ module datapath #(parameter WIDTH = 8, REGBITS = 3)
   always_comb 
     tries_eq_7 = (tries == 3'b111);
   
-  always_comb 
+  always_ff @(posedge clk)
     if (en_win) win = s_win;
 
-  always_comb 
+  always_ff @(posedge clk)
     if (en_lose) lose = s_lose;  
 endmodule
 
